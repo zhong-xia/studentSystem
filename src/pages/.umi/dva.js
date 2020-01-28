6f426d393542b05ce6b4e5b289591722e46fc96b
zhongxia_1 <1641156234@qq.com>
2020-01-28 14:34:16 +0800

    学生管理系统

---
+++ src/pages/.umi/dva.js
@@ -0,0 +1,38 @@
+import dva from 'dva';
+import { Component } from 'react';
+import createLoading from 'dva-loading';
+import history from '@tmp/history';
+
+let app = null;
+
+export function _onCreate() {
+  const plugins = require('umi/_runtimePlugin');
+  const runtimeDva = plugins.mergeConfig('dva');
+  app = dva({
+    history,
+    
+    ...(runtimeDva.config || {}),
+    ...(window.g_useSSR ? { initialState: window.g_initialData } : {}),
+  });
+  
+  app.use(createLoading());
+  (runtimeDva.plugins || []).forEach(plugin => {
+    app.use(plugin);
+  });
+  
+  app.model({ namespace: 'loginUser', ...(require('C:/Users/纪雪梅/Desktop/1/DuYi-React/antd-learn/src/models/loginUser.js').default) });
+app.model({ namespace: 'students', ...(require('C:/Users/纪雪梅/Desktop/1/DuYi-React/antd-learn/src/models/students.js').default) });
+  return app;
+}
+
+export function getApp() {
+  return app;
+}
+
+export class _DvaContainer extends Component {
+  render() {
+    const app = getApp();
+    app.router(() => this.props.children);
+    return app.start()();
+  }
+}
---
+++ src/pages/.umi/router.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+  Router as DefaultRouter,
+  Route,
+  Switch,
+  StaticRouter,
+} from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/lib/renderRoutes';
+import history from '@@/history';
+import { routerRedux } from 'dva';
+
+const Router = routerRedux.ConnectedRouter;
+
+const routes = [
+  {
+    path: '/',
+    component: require('../../layouts/index.js').default,
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        component: require('../index.js').default,
+        Routes: [require('../../router/PrivateRouter').default],
+      },
+      {
+        path: '/login',
+        exact: true,
+        component: require('../login.js').default,
+      },
+      {
+        path: '/student/add',
+        exact: true,
+        component: require('../student/add.js').default,
+        Routes: [require('../../router/PrivateRouter').default],
+      },
+      {
+        path: '/student',
+        exact: true,
+        component: require('../student/index.js').default,
+        Routes: [require('../../router/PrivateRouter').default],
+      },
+      {
+        path: '/student/:id',
+        exact: true,
+        component: require('../student/$id.js').default,
+        Routes: [require('../../router/PrivateRouter').default],
+      },
+      {
+        component: () =>
+          React.createElement(
+            require('C:/Users/纪雪梅/AppData/Roaming/npm/node_modules/umi/node_modules/umi-build-dev/lib/plugins/404/NotFound.js')
+              .default,
+            { pagesPath: 'src/pages', hasRoutesInConfig: false },
+          ),
+      },
+    ],
+  },
+  {
+    component: () =>
+      React.createElement(
+        require('C:/Users/纪雪梅/AppData/Roaming/npm/node_modules/umi/node_modules/umi-build-dev/lib/plugins/404/NotFound.js')
+          .default,
+        { pagesPath: 'src/pages', hasRoutesInConfig: false },
+      ),
+  },
+];
+window.g_routes = routes;
+const plugins = require('umi/_runtimePlugin');
+plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+export { routes };
+
+export default class RouterWrapper extends React.Component {
+  unListen() {}
+
+  constructor(props) {
+    super(props);
+
+    // route change handler
+    function routeChangeHandler(location, action) {
+      plugins.applyForEach('onRouteChange', {
+        initialValue: {
+          routes,
+          location,
+          action,
+        },
+      });
+    }
+    this.unListen = history.listen(routeChangeHandler);
+    // dva 中 history.listen 会初始执行一次
+    // 这里排除掉 dva 的场景，可以避免 onRouteChange 在启用 dva 后的初始加载时被多执行一次
+    const isDva =
+      history.listen
+        .toString()
+        .indexOf('callback(history.location, history.action)') > -1;
+    if (!isDva) {
+      routeChangeHandler(history.location);
+    }
+  }
+
+  componentWillUnmount() {
+    this.unListen();
+  }
+
+  render() {
+    const props = this.props || {};
+    return <Router history={history}>{renderRoutes(routes, props)}</Router>;
+  }
+}
